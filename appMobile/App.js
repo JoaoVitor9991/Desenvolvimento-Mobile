4f3f6612afa2c0e88abfab2e0cf89672382338c8
joão Vitor <joaovitormachadodev99@gmail.com>
2025-04-09 17:49:41 -0300

    att

--- appMobile/App.js
+++ appMobile/App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    alert('Você Ganhou 200 Milhões');
+  };
+
+
+
   return (
     <View style={styles.container}>
       <Text>João Vitor</Text>
-      
+      <Image
+        style ={styles.Image}
+        source={require('./Imagem/imagem.jpg')}
+      />
+      <Button
+      title='Press Here'
+      onPress={handlePress}
+      />
     </View>
   );
 }
