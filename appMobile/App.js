5654761433bb217d1e8b379aabcacf446296574e
joão Vitor <joaovitormachadodev99@gmail.com>
2025-04-14 15:28:57 -0300

    att

--- appMobile/App.js
+++ appMobile/App.js
@@ -1,24 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+function MinhasCredenciais({ githubUser }) {
+  return (
+    <View style={styles.credenciaisContainer}>
+      <View style={{ flexDirection: 'row', padding: 15, alignItems: 'center' }}>
+        <Image
+          style={{
+            marginRight: 14,
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+          }}
+          source={{
+            uri: `https://github.com/${githubUser}.png`,
+          }}
+        />
+        <View>
+          <AntDesign name="hearto" size={24} color="black" />
+        </View>
+      </View>
+    </View>
+  );
+}
 
 export default function App() {
-  const handlePress = () => {
-    alert('Você Ganhou 200 Milhões');
-  };
-
-
-
   return (
     <View style={styles.container}>
-      <Text>João Vitor</Text>
-      <Image
-        style ={styles.Image}
-        source={require('./Imagem/imagem.jpg')}
-      />
-      <Button
-      title='Press Here'
-      onPress={handlePress}
-      />
+      {[
+        'WRK7',
+        'mauriiicio',
+        'anneprimonn',
+        'joaovitor9991'
+
+      ].map((githubUser) => (
+        <MinhasCredenciais key={githubUser} githubUser={githubUser} />
+      ))}
     </View>
   );
 }
@@ -26,7 +44,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#f2f2f2',
+  },
+  credenciaisContainer: {
+    marginBottom: 20,
+  },
+  text: {
+    color: '#fff',
+    marginRight: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
