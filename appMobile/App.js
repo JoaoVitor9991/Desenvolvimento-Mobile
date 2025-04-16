06ca8e5244f7610f80d057891b81f85986e64326
joão Vitor <joaovitormachadodev99@gmail.com>
2025-04-16 18:18:29 -0300

    att

--- appMobile/App.js
+++ appMobile/App.js
@@ -63,7 +63,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff',
     //alignItems: 'center',
     //justifyContent: 'center',
   },
