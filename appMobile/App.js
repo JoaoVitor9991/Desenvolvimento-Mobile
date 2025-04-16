bc1fda6280acad7a613f969bd8fe196a78da8023
joão Vitor <joaovitormachadodev99@gmail.com>
2025-04-16 17:22:32 -0300

    att

--- appMobile/App.js
+++ appMobile/App.js
@@ -1,73 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image, SafeAreaView } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { ScrollView } from 'react-native-web';
+import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView} from 'react-native';
 
 function MinhasCredenciais({ githubUser }) {
   return (
-    <SafeAreaView style={styles.credenciaisContainer}>
+    <View>
       <View style={{ flexDirection: 'row', padding: 15, alignItems: 'center' }}>
         <Image
           style={{
-            marginRight: 14,
+            marginRight: 15,
             width: 40,
             height: 40,
-            borderRadius: 20,
+            bordeRadius: 20,
           }}
           source={{
-            uri: `https://github.com/${githubUser}.png`,
+            uri: `https://github.com/${githubUser}.png `
           }}
         />
-        <Image
-          style={{
-            width:"400px",
-            height: 350 
-            
-          }}
-     
-          source={{
-            uri: `https://github.com/${githubUser}.png`,
-          }}
-        />
-        <Text >{githubUser}</Text>
-        
-        <View>
-          <AntDesign name="hearto" size={24} color="black" />
-        </View>
+        <Text>{githubUser}</Text>
       </View>
-    </SafeAreaView>
-  );
+      <Image
+        style={{
+          width: '100%',
+          height: 350
+        }}
+        source={{
+          uri: `https://github.com/${githubUser}.png `
+        }}
+      />
+      <View>
+        <AntDesign onPress={AntDesign} name="heart" size={24} color="black" />
+      </View>
+    </View>
+
+  )
 }
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ScrollView>
-      {[
-        'WRK7',
-        'mauriiicio',
-        'anneprimonn',
-        'joaovitor9991'
-
-      ].map((githubUser) => (
-        <MinhasCredenciais key={githubUser} githubUser={githubUser} />
-      ))}
+        {[
+          'mauriiicio',
+          'eulampio',
+          'celsolucas',
+          'anneprimonn',
+          'joaovitor9991'
+        ].map((githubUser) => <MinhasCredenciais
+          key={githubUser} githubUser={githubUser} />
+        )}
       </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f2f2f2',
-  },
-  credenciaisContainer: {
-    marginBottom: 20,
-  },
-  text: {
-    color: '#fff',
-    marginRight: 10,
+    backgroundColor: '#fff',
     //alignItems: 'center',
     //justifyContent: 'center',
   },
