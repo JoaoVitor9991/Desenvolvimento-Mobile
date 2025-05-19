2acb4949fe45e6083b783b0940cdb25a89c1da97
joão Vitor <joaovitormachadodev99@gmail.com>
2025-05-19 14:42:27 -0300

    att

--- appMobile/App.js
+++ appMobile/App.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    //alignItems: 'cente',
+    //alignItems: 'center',
     //justifyContent: 'center',
   },
 });
