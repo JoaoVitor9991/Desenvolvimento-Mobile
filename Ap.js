81df9a91433d100939004ff61f0f0b2de6d54579
joão Vitor <joaovitormachadodev99@gmail.com>
2025-06-30 17:34:02 -0300

    att

--- Ap.js
+++ Ap.js
@@ -1,64 +1,8 @@
-import AntDesign from '@expo/vector-icons/AntDesign';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView} from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Modal, KeyboardAvoidingView, Platform} from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign'
 
-function MinhasCredenciais({ githubUser }) {
-  const[isLiked, setIsLiked] = useState(false);
-  return (
-    <View>
-      <View style={{ flexDirection: 'row', padding: 15, alignItems: 'center' }}>
-        <Image
-          style={{
-            marginRight: 15,
-            width: 40,
-            height: 40,
-            borderRadius: 20,
-          }}
-          source={{
-            uri: `https://github.com/${githubUser}.png `
-          }}
-        />
-        <Text>{githubUser}</Text>
-      </View>
-      <Image
-        style={{
-          width: '100%',
-          height: 350
-        }}
-        source={{
-          uri: `https://github.com/${githubUser}.png `
-        }}
-      />
-      <View>
-        <AntDesign 
-        onPress={() => setIsLiked(!isLiked)}
-        name={isLiked? "heart" : "hearto"}
-        size={24}
-        color={isLiked? "red" : "black"} 
-        />
-      </View>
-    </View>
 
-  )
-}
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        {[
-          'mauriiicio',
-          'eulampio',
-          'celsolucas',
-          'anneprimonn',
-          'joaovitor9991'
-        ].map((githubUser) => <MinhasCredenciais
-          key={githubUser} githubUser={githubUser} />
-        )}
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -137,5 +81,5 @@ const styles = StyleSheet.create({
     fontWeight: '600',
   },
 
-  
+
 });
