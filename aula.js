b0fd4526e949f28f93952604a4625aa5f1bdb421
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-23 15:30:43 -0300

    a

--- aula.js
+++ aula.js
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Mauricio</Text>
+      <Text>Mauricios</Text>
       
       <Button
         title='Press Here'
